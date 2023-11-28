25902bdbe986f6953f967422fe0f872b370fc153
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-11-28 14:44:15 -0800

    chore: update script to take input file prompts

--- model-generated/distilgpt2-emailgen-phishing/distilgpt2-emailgen-phishing.py
+++ model-generated/distilgpt2-emailgen-phishing/distilgpt2-emailgen-phishing.py
@@ -4,17 +4,24 @@ tokenizer = GPT2Tokenizer.from_pretrained('postbot/distilgpt2-emailgen-V2')
 model = GPT2LMHeadModel.from_pretrained(
     "loresiensis/distilgpt2-emailgen-phishing")
 
-# Generate text
-input_text = "Dear customer,"
-input_ids = tokenizer.encode(input_text, return_tensors='pt')
+# Prompt the user for the file containing line-separated prompts
+prompt_file_path = input("Enter the file path and name of the prompts file: ")
 
-output = model.generate(input_ids, max_length=300,
-                        temperature=0.7, do_sample=True)
+# Read prompts from the file
+with open(prompt_file_path, 'r') as prompt_file:
+    prompts = prompt_file.read().splitlines()
 
-output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-print(output_text)
+# Generate text for each prompt
+for prompt in prompts:
+    input_ids = tokenizer.encode(prompt, return_tensors='pt')
 
-# Append the output to a file
-output_file_path = "generated_text.txt"
-with open(output_file_path, 'a') as file:
-    file.write(output_text + '\n')
+    output = model.generate(input_ids, max_length=300,
+                            temperature=0.7, do_sample=True)
+
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(output_text)
+
+    # Append the output to a file
+    output_file_path = "generated_text.txt"
+    with open(output_file_path, 'a') as file:
+        file.write(output_text + '\n')
