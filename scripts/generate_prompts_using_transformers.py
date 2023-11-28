7154a45b16c520ebaf0ae97a9b7d561f77be2699
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-11-28 14:22:28 -0800

    chore: increase gen length

--- scripts/generate_prompts_using_transformers.py
+++ scripts/generate_prompts_using_transformers.py
@@ -1,9 +1,11 @@
 from transformers import pipeline
 
+
 def read_prompts_from_file(file_path):
     with open(file_path, 'r') as file:
         prompts = file.read().splitlines()
     return prompts
+
 
 def generate_responses(model_name, prompts):
     # Load the model from Hugging Face
@@ -15,25 +17,29 @@ def generate_responses(model_name, prompts):
     # Generate responses for each prompt
     for prompt in prompts:
         # Generate response using the loaded model
-        response = generator(prompt, max_length=50, num_return_sequences=1)
+        response = generator(prompt, max_length=300, num_return_sequences=1)
 
         # Append the response to the list
         responses.append(response[0]['generated_text'].strip())
 
     return responses
 
+
 def main():
     # Input file path for line-separated prompts
-    prompts_file_path = input("Enter the file path containing line-separated prompts: ")
+    prompts_file_path = input(
+        "Enter the file path containing line-separated prompts: ")
 
     # Read prompts from the specified file
     prompts = read_prompts_from_file(prompts_file_path)
 
     # Input model name from Hugging Face
-    model_name = input("Enter the model name from Hugging Face (e.g., 'EleutherAI/gpt-neo-1.3B'): ")
+    model_name = input(
+        "Enter the model name from Hugging Face (e.g., 'EleutherAI/gpt-neo-1.3B'): ")
 
     # Input destination file for writing the results
-    output_file = input("Enter the destination file to write the results to (e.g., 'output.txt'): ")
+    output_file = input(
+        "Enter the destination file to write the results to (e.g., 'output.txt'): ")
 
     # Generate responses using the specified model and prompts
     responses = generate_responses(model_name, prompts)
@@ -45,6 +51,6 @@ def main():
 
     print(f"Responses written to {output_file}")
 
+
 if __name__ == "__main__":
     main()
-
